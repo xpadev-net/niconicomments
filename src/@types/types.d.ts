1213f0a89597fa5c8a1e7caf31c7f2238dbfdf8c
xpadev-net <xpadev@gmail.com>
2022-07-27 18:39:53 +0900

    clean up and refactor

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -1,154 +1,226 @@
-type inputFormatType = "niconicome"|"formatted"|"legacy"|"owner"|"v1"|"default";
+type inputFormatType =
+  | "niconicome"
+  | "formatted"
+  | "legacy"
+  | "owner"
+  | "v1"
+  | "default";
 type InitOptions = {
-    useLegacy?: boolean,
-    formatted?: boolean,
-    format?: inputFormatType,
-    video?: HTMLVideoElement | null
-    showCollision?: boolean,
-    showFPS?: boolean,
-    showCommentCount?: boolean,
-    drawAllImageOnLoad?: boolean,
-    debug?: boolean,
-    enableLegacyPiP?: boolean,
-    keepCA?: boolean
-}
+  useLegacy?: boolean;
+  formatted?: boolean;
+  format?: inputFormatType;
+  video?: HTMLVideoElement | null;
+  showCollision?: boolean;
+  showFPS?: boolean;
+  showCommentCount?: boolean;
+  drawAllImageOnLoad?: boolean;
+  debug?: boolean;
+  enableLegacyPiP?: boolean;
+  keepCA?: boolean;
+};
 type Options = {
-    useLegacy: boolean,
-    formatted: boolean,
-    format: inputFormatType,
-    video: HTMLVideoElement | null,
-    showCollision: boolean,
-    showFPS: boolean,
-    showCommentCount: boolean,
-    drawAllImageOnLoad: boolean,
-    debug: boolean,
-    enableLegacyPiP: boolean,
-    keepCA: boolean
-}
+  useLegacy: boolean;
+  formatted: boolean;
+  format: inputFormatType;
+  video: HTMLVideoElement | null;
+  showCollision: boolean;
+  showFPS: boolean;
+  showCommentCount: boolean;
+  drawAllImageOnLoad: boolean;
+  debug: boolean;
+  enableLegacyPiP: boolean;
+  keepCA: boolean;
+};
 type rawApiResponse = {
-    [key: string]: apiPing | apiThread | apiLeaf | apiGlobalNumRes | apiChat
-}
+  [key: string]: apiPing | apiThread | apiLeaf | apiGlobalNumRes | apiChat;
+};
 type apiPing = {
-    "content": string
-}
+  content: string;
+};
 type apiThread = {
-    "resultcode": number,
-    "thread": string,
-    "server_time": number,
-    "ticket": string,
-    "revision": number
-}
+  resultcode: number;
+  thread: string;
+  server_time: number;
+  ticket: string;
+  revision: number;
+};
 type apiLeaf = {
-    "thread": string,
-    "count": number
-}
+  thread: string;
+  count: number;
+};
 type apiGlobalNumRes = {
-    "thread": string,
-    "num_res": number
-}
+  thread: string;
+  num_res: number;
+};
 type apiChat = {
-    "thread": string,
-    "no": number,
-    "vpos": number,
-    "date": number,
-    "date_usec": number,
-    "nicoru": number,
-    "premium": number,
-    "anonymity": number,
-    "user_id": string,
-    "mail": string,
-    "content": string,
-    "deleted": number
-}
+  chat: {
+    thread: string;
+    no: number;
+    vpos: number;
+    date: number;
+    date_usec: number;
+    nicoru: number;
+    premium: number;
+    anonymity: number;
+    user_id: string;
+    mail: string;
+    content: string;
+    deleted: number;
+  };
+};
 type formattedComment = {
-    "id": number,
-    "vpos": number,
-    "content": string,
-    "date": number,
-    "date_usec": number,
-    "owner": boolean,
-    "premium": boolean,
-    "mail": string[],
-    "user_id": number,
-    "layer": number,
-}
+  id: number;
+  vpos: number;
+  content: string;
+  date: number;
+  date_usec: number;
+  owner: boolean;
+  premium: boolean;
+  mail: string[];
+  user_id: number;
+  layer: number;
+};
 type formattedLegacyComment = {
-    "id": number,
-    "vpos": number,
-    "content": string,
-    "date": number,
-    "date_usec": number,
-    "owner": boolean,
-    "premium": boolean,
-    "mail": string[],
-}
+  id: number;
+  vpos: number;
+  content: string;
+  date: number;
+  date_usec: number;
+  owner: boolean;
+  premium: boolean;
+  mail: string[];
+};
 type formattedCommentWithFont = formattedComment & {
-    "loc": string,
-    "size": string,
-    "fontSize": number,
-    "font": string,
-    "color": string,
-    "full": boolean,
-    "ender": boolean,
-    "_live": boolean,
-    "long": number,
-    "invisible": boolean
-}
+  loc: commentLoc;
+  size: commentSize;
+  fontSize: number;
+  font: commentFont;
+  color: string;
+  full: boolean;
+  ender: boolean;
+  _live: boolean;
+  long: number;
+  invisible: boolean;
+};
 type formattedCommentWithSize = formattedCommentWithFont & {
-    "height": number,
-    "width": number,
-    "width_max": number,
-    "width_min": number,
-    "lineHeight": number
-}
+  height: number;
+  width: number;
+  width_max: number;
+  width_min: number;
+  lineHeight: number;
+};
 type parsedComment = formattedCommentWithSize & {
-    posY: number,
-    image?: HTMLCanvasElement | boolean
-}
+  posY: number;
+  image?: HTMLCanvasElement | boolean;
+};
+type groupedResult = formattedCommentWithFont & {
+  index: number;
+};
+type groupedComments = {
+  [key in commentFont]: { [key: string]: groupedResult[] };
+};
+type commentFont = "defont" | "mincho" | "gothic";
+type commentSize = "big" | "medium" | "small";
+type commentLoc = "ue" | "naka" | "shita";
+type posCollision = { [p: number]: number[] };
+type collision = { [key in collisionPosList]: posCollision };
+type collisionPosList = "ue" | "shita" | "right" | "left";
+type nicoScript = {
+  reverse: nicoScriptReverse[];
+  ban: nicoScriptBan[];
+  default: nicoScriptDefault[];
+  replace: nicoScriptReplace[];
+};
+type nicoScriptReverse = {
+  target: nicoScriptReverseTarget;
+  start: number;
+  end: number;
+};
+type nicoScriptReverseTarget = "コメ" | "投コメ" | "全";
+type nicoScriptReplace = {
+  start: number;
+  long: number | null;
+  keyword: string;
+  replace: string;
+  range: nicoScriptReplaceRange;
+  target: nicoScriptReplaceTarget;
+  condition: nicoScriptReplaceCondition;
+  color: string | null;
+  size: commentSize | null;
+  font: commentFont | null;
+  loc: commentLoc | null;
+};
+type nicoScriptReplaceRange = "単" | "全";
+type nicoScriptReplaceTarget = "コメ" | "投コメ" | "全" | "含まない" | "含む";
+type nicoScriptReplaceCondition = "完全一致" | "部分一致";
+type nicoScriptBan = {
+  start: number;
+  end: number;
+};
+type nicoScriptDefault = {
+  start: number;
+  long: number | null;
+  color: string | null;
+  size: commentSize | null;
+  font: commentFont | null;
+  loc: commentLoc | null;
+};
 type measureTextResult = {
-    "width": number,
-    "width_max": number,
-    "width_min": number,
-    "height": number,
-    "resized": boolean,
-    "fontSize": number,
-    "lineHeight": number
-}
+  width: number;
+  width_max: number;
+  width_min: number;
+  height: number;
+  resized: boolean;
+  fontSize: number;
+  lineHeight: number;
+};
+type parsedCommand = {
+  loc: commentLoc | null;
+  size: commentSize | null;
+  fontSize: number | null;
+  color: string | null;
+  font: commentFont | null;
+  full: boolean;
+  ender: boolean;
+  _live: boolean;
+  invisible: boolean;
+  long: number | null;
+};
 type T_fontSize = {
-    [key: string]: {
-        "default": number,
-        "resized": number
-    }
-}
+  [key in commentSize]: {
+    default: number;
+    resized: number;
+  };
+};
 type T_doubleResizeMaxWidth = {
-    [key: string]: {
-        "legacy": number,
-        "default": number
-    }
-}
+  [key in "full" | "normal"]: {
+    legacy: number;
+    default: number;
+  };
+};
 type v1Thread = {
-    "id": string,
-    "fork": string,
-    "commentCount": number,
-    "comments": { [key: string]:v1Comment }
-}
+  id: string;
+  fork: string;
+  commentCount: number;
+  comments: { [key: string]: v1Comment };
+};
 type v1Comment = {
-    "id": string,
-    "no": number,
-    "vposMs": number,
-    "body": string,
-    "commands": string[],
-    "userId": string,
-    "isPremium": boolean,
-    "score": number,
-    "postedAt": string,
-    "nicoruCount": number,
-    "nicoruId": null,
-    "source": string,
-    "isMyPost": boolean
-}
+  id: string;
+  no: number;
+  vposMs: number;
+  body: string;
+  commands: string[];
+  userId: string;
+  isPremium: boolean;
+  score: number;
+  postedAt: string;
+  nicoruCount: number;
+  nicoruId: null;
+  source: string;
+  isMyPost: boolean;
+};
 type ownerComment = {
-    "time": string,
-    "command": string,
-    "comment": string
-}
+  time: string;
+  command: string;
+  comment: string;
+};
--- src/typeGuard.ts
+++ src/typeGuard.ts
@@ -1,114 +1,224 @@
 const typeGuard = {
-    formatted: {
-        comment: (i: any): i is formattedComment =>
-            typeVerify(i, ["id", "vpos", "content", "date", "date_usec", "owner", "premium", "mail", "user_id", "layer"]),
-        comments: (i: any): i is formattedComment[] => {
-            if (typeof i !== "object") return false;
-            for (let item of i) {
-                if (!typeGuard.formatted.comment(item)) return false;
-            }
-            return true;
-        },
-        legacyComment: (i: any): i is formattedLegacyComment =>
-            typeVerify(i, ["id", "vpos", "content", "date", "owner", "premium", "mail"]),
-        legacyComments: (i: any): i is formattedLegacyComment[] => {
-            if (typeof i !== "object") return false;
-            for (let item of i) {
-                if (!typeGuard.formatted.legacyComment(item)) return false;
-            }
-            return true;
-        }
+  formatted: {
+    comment: (i: unknown): i is formattedComment =>
+      typeVerify(i, [
+        "id",
+        "vpos",
+        "content",
+        "date",
+        "date_usec",
+        "owner",
+        "premium",
+        "mail",
+        "user_id",
+        "layer",
+      ]),
+    comments: (i: unknown): i is formattedComment[] => {
+      if (typeof i !== "object") return false;
+      for (const item of i as formattedComment[]) {
+        if (!typeGuard.formatted.comment(item)) return false;
+      }
+      return true;
     },
-    legacy: {
-        rawApiResponses: (i: any): i is rawApiResponse[] => {
-            if (typeof i !== "object") return false;
-            for (let itemWrapper of i) {
-                for (let item of itemWrapper){
-                    if (!(item.chat&&typeGuard.legacy.apiChat(item) || typeGuard.legacy.apiGlobalNumRes(item) || typeGuard.legacy.apiLeaf(item) || typeGuard.legacy.apiPing(item) || typeGuard.legacy.apiThread(item))) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        },
-        apiChat: (i: any): i is apiChat =>
-            typeVerify(i, ["anonymity", "content", "date", "date_usec", "no", "thread", "vpos"]),
-        apiGlobalNumRes: (i: any): i is apiGlobalNumRes =>
-            typeVerify(i, ["num_res", "thread"]),
-        apiLeaf: (i: any): i is apiLeaf =>
-            typeVerify(i, ["count", "thread"]),
-        apiPing: (i: any): i is apiPing =>
-            typeVerify(i, ["content"]),
-        apiThread: (i: any): i is apiThread =>
-            typeVerify(i, ["resultcode", "revision", "server_time", "thread", "ticket"]),
+    legacyComment: (i: unknown): i is formattedLegacyComment =>
+      typeVerify(i, [
+        "id",
+        "vpos",
+        "content",
+        "date",
+        "owner",
+        "premium",
+        "mail",
+      ]),
+    legacyComments: (i: unknown): i is formattedLegacyComment[] => {
+      if (typeof i !== "object") return false;
+      for (const item of i as formattedLegacyComment[]) {
+        if (!typeGuard.formatted.legacyComment(item)) return false;
+      }
+      return true;
     },
-    niconicome: {
-        xmlDocument: (i: any): i is XMLDocument => {
-            if (!i.documentElement || i.documentElement.nodeName !== "packet") return false;
-            if (!i.documentElement.children) return false;
-            for (let index in Array.from(i.documentElement.children)) {
-                let value = i.documentElement.children[index];
-                if (index === "0") {
-                    if (value.nodeName !== "thread" || !typeAttributeVerify(value, ["resultcode", "thread", "server_time", "last_res", "revision"])) return false;
-                } else {
-                    if (value.nodeName !== "chat" || !typeAttributeVerify(value, ["thread", "no", "vpos", "date", "date_usec", "anonymity", "user_id", "mail", "leaf", "premium", "score"])) return false;
-                }
-            }
-            return true;
+  },
+  legacy: {
+    rawApiResponses: (i: unknown): i is rawApiResponse[] => {
+      if (typeof i !== "object") return false;
+      for (const itemWrapper of i as rawApiResponse[]) {
+        for (const key of Object.keys(itemWrapper)) {
+          const item = itemWrapper[key];
+          if (!item) continue;
+          if (
+            !(
+              typeGuard.legacy.apiChat(item) ||
+              typeGuard.legacy.apiGlobalNumRes(item) ||
+              typeGuard.legacy.apiLeaf(item) ||
+              typeGuard.legacy.apiPing(item) ||
+              typeGuard.legacy.apiThread(item)
+            )
+          ) {
+            return false;
+          }
         }
+      }
+      return true;
     },
-    legacyOwner:{
-        comments: (i: any): boolean => {
-            let lists = i.split("\n");
-            for (let list in lists){
-                if(list.split(":").length<3){
-                    return false;
-                }
-            }
-            return true;
+    apiChat: (i: unknown): i is apiChat =>
+      typeof i === "object" &&
+      (i as apiChat).chat &&
+      typeVerify((i as apiChat).chat, [
+        "anonymity",
+        "content",
+        "date",
+        "date_usec",
+        "no",
+        "thread",
+        "vpos",
+      ]),
+    apiGlobalNumRes: (i: unknown): i is apiGlobalNumRes =>
+      typeVerify(i, ["num_res", "thread"]),
+    apiLeaf: (i: unknown): i is apiLeaf => typeVerify(i, ["count", "thread"]),
+    apiPing: (i: unknown): i is apiPing => typeVerify(i, ["content"]),
+    apiThread: (i: unknown): i is apiThread =>
+      typeVerify(i, [
+        "resultcode",
+        "revision",
+        "server_time",
+        "thread",
+        "ticket",
+      ]),
+  },
+  niconicome: {
+    xmlDocument: (i: unknown): i is XMLDocument => {
+      if (
+        !(i as XMLDocument).documentElement ||
+        (i as XMLDocument).documentElement.nodeName !== "packet"
+      )
+        return false;
+      if (!(i as XMLDocument).documentElement.children) return false;
+      for (
+        let index = 0;
+        index < (i as XMLDocument).documentElement.children.length;
+        index++
+      ) {
+        const value = (i as XMLDocument).documentElement.children[index];
+        if (!value) continue;
+        if (index === 0) {
+          if (
+            value.nodeName !== "thread" ||
+            !typeAttributeVerify(value, [
+              "resultcode",
+              "thread",
+              "server_time",
+              "last_res",
+              "revision",
+            ])
+          )
+            return false;
+        } else {
+          if (
+            value.nodeName !== "chat" ||
+            !typeAttributeVerify(value, [
+              "thread",
+              "no",
+              "vpos",
+              "date",
+              "date_usec",
+              "anonymity",
+              "user_id",
+              "mail",
+              "leaf",
+              "premium",
+              "score",
+            ])
+          )
+            return false;
         }
+      }
+      return true;
     },
-    owner: {
-        comment: (i: any): i is ownerComment =>
-            typeVerify(i, ["time", "command", "comment"]),
-        comments: (i: any): i is ownerComment[] => {
-            if (typeof i !== "object") return false;
-            for (let item of i) {
-                if (!typeGuard.owner.comment(item)) return false;
-            }
-            return true;
+  },
+  legacyOwner: {
+    comments: (i: unknown): boolean => {
+      if (typeof i !== "string") return false;
+      const lists = i.split("\n");
+      for (const list of lists) {
+        if (list.split(":").length < 3) {
+          return false;
         }
+      }
+      return true;
+    },
+  },
+  owner: {
+    comment: (i: unknown): i is ownerComment =>
+      typeVerify(i, ["time", "command", "comment"]),
+    comments: (i: unknown): i is ownerComment[] => {
+      if (typeof i !== "object") return false;
+      for (const item of i as ownerComment[]) {
+        if (!typeGuard.owner.comment(item)) return false;
+      }
+      return true;
+    },
+  },
+  v1: {
+    comment: (i: unknown): i is apiThread =>
+      typeVerify(i, [
+        "id",
+        "no",
+        "vposMs",
+        "body",
+        "commands",
+        "userId",
+        "isPremium",
+        "score",
+        "postedAt",
+        "nicoruCount",
+        "nicoruId",
+        "source",
+        "isMyPost",
+      ]),
+    thread: (i: unknown): i is v1Thread => {
+      if (!typeVerify(i, ["id", "fork", "commentCount", "comments"]))
+        return false;
+      for (const item of Object.keys((i as v1Thread).comments)) {
+        if (!typeGuard.v1.comment((i as v1Thread).comments[item])) return false;
+      }
+      return true;
+    },
+    threads: (i: unknown): i is v1Thread[] => {
+      if (typeof i !== "object") return false;
+      for (const item of i as v1Thread[]) {
+        if (!typeGuard.v1.thread(item)) return false;
+      }
+      return true;
+    },
+  },
 
+  nicoScript: {
+    range: {
+      target: (i: unknown): i is nicoScriptReverseTarget =>
+        typeof i === "string" && !!i.match(/^コメ|投コメ|全$/),
     },
-    v1: {
-        comment: (i: any): i is apiThread =>
-            typeVerify(i, ["id", "no", "vposMs", "body", "commands", "userId", "isPremium", "score", "postedAt", "nicoruCount", "nicoruId", "source", "isMyPost"]),
-        thread: (i: any): i is v1Thread => {
-            if (!typeVerify(i, ["id", "fork", "commentCount", "comments"])) return false;
-            for (let item of i.comments) {
-                if (!typeGuard.v1.comment(item)) return false;
-            }
-            return true;
-        },
-        threads: (i: any): i is v1Thread[] => {
-            if (typeof i !== "object") return false;
-            for (let item of i) {
-                if (!typeGuard.v1.thread(item)) return false;
-            }
-            return true;
-        },
-    }
-}
-const typeVerify = (item: any, keys: string[]): boolean => {
-    for (let key of keys) {
-        if (item[key] === undefined) return false;
-    }
-    return true
-}
-const typeAttributeVerify = (item: any, keys: string[]): boolean => {
-    for (let key of keys) {
-        if (item.getAttribute(key) === null) return false;
-    }
-    return true
-}
+    replace: {
+      range: (i: unknown): i is nicoScriptReplaceRange =>
+        typeof i === "string" && !!i.match(/^単|全$/),
+      target: (i: unknown): i is nicoScriptReplaceTarget =>
+        typeof i === "string" && !!i.match(/^コメ|投コメ|全|含む|含まない$/),
+      condition: (i: unknown): i is nicoScriptReplaceCondition =>
+        typeof i === "string" && !!i.match(/^部分一致|完全一致$/),
+    },
+  },
+};
+const typeVerify = (item: unknown, keys: string[]): boolean => {
+  if (typeof item !== "object" || !item) return false;
+  for (const key of keys) {
+    if (Object.prototype.hasOwnProperty.call(item, key)) return false;
+  }
+  return true;
+};
+const typeAttributeVerify = (item: unknown, keys: string[]): boolean => {
+  if (typeof item !== "object" || !item) return false;
+  for (const key of keys) {
+    if ((item as Element).getAttribute(key) === null) return false;
+  }
+  return true;
+};
 export default typeGuard;

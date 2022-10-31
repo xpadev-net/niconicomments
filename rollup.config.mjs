1c4ad128220482f901b1fff4a9d652f48b9c3851
xpa <xpadev@gmail.com>
2022-11-01 02:29:08 +0900

    update: dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,17 @@
 import babel from "@rollup/plugin-babel";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import pkg from "./package.json";
+import pkg from "./package.json" assert {type: 'json'};
+import { fileURLToPath } from 'url';
 import * as path from "path";
 const banner = `/*!
   niconicomments.js v${pkg.version}
   (c) 2021 xpadev-net https://xpadev.net
   Released under the ${pkg.license} License.
 */`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export default {
     input: 'src/main.ts',

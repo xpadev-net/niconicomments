4b6049e12e84addf430f347917055b94bfc17914
xpa <xpadev@gmail.com>
2022-11-01 15:23:56 +0900

    fix: incorrect scope variable

--- src/contexts/canvas.ts
+++ src/contexts/canvas.ts
@@ -4,7 +4,7 @@ const initCanvas = () => {
   canvas = document.createElement("canvas");
   const _context = canvas.getContext("2d");
   if (_context === null) throw new Error("fail to get Context2D");
-  const context = _context;
+  context = _context;
   context.textAlign = "start";
   context.textBaseline = "alphabetic";
 };

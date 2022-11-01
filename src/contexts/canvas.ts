bcf07356f072bb93b0cdc462e8c6c98f7d4639ff
xpa <xpadev@gmail.com>
2022-11-01 14:40:25 +0900

    fix: fail when import this library in node

--- src/contexts/canvas.ts
+++ src/contexts/canvas.ts
@@ -1,7 +1,11 @@
-const canvas = document.createElement("canvas");
-const _context = canvas.getContext("2d");
-if (_context === null) throw new Error("fail to get Context2D");
-const context = _context;
-context.textAlign = "start";
-context.textBaseline = "alphabetic";
-export { canvas, context };
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+const initCanvas = () => {
+  canvas = document.createElement("canvas");
+  const _context = canvas.getContext("2d");
+  if (_context === null) throw new Error("fail to get Context2D");
+  const context = _context;
+  context.textAlign = "start";
+  context.textBaseline = "alphabetic";
+};
+export { canvas, context, initCanvas };

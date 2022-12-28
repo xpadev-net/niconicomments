930bb588abbe70158a33b50b5b38073688b655c2
xpa <xpadev@gmail.com>
2022-12-28 19:48:49 +0900

    fix: typeguard

--- src/typeGuard.ts
+++ src/typeGuard.ts
@@ -240,7 +240,7 @@ const typeGuard = {
         format: (i: unknown) =>
           typeof i === "string" &&
           !!i.match(
-            /^(niconicome|formatted|legacy|legacyOwner|owner|v1|default)$/
+            /^(niconicome|formatted|legacy|legacyOwner|owner|v1|default|empty)$/
           ),
         video: (i: unknown) =>
           typeof i === "object" && (i as HTMLVideoElement).nodeName === "VIDEO",

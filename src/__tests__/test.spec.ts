17d329be15c9557d551ac16b98edae87cfea8583
xpa <xpadev@gmail.com>
2023-05-05 14:06:39 +0900

    improve: add playwright

--- src/__tests__/test.spec.ts
+++ src/__tests__/test.spec.ts
@@ -8,8 +8,29 @@ test("7(to the beginning)", async ({ page }) => {
   await compare(page, 7, 60);
 });
 
+test("8(満天)", async ({ page }) => {
+  await compare(page, 8, 50);
+  await compare(page, 8, 80);
+  await compare(page, 8, 90);
+});
+
+test("9(うっせぇわ)", async ({ page }) => {
+  await compare(page, 9, 70);
+});
+
+test("11(アンインストール)", async ({ page }) => {
+  await compare(page, 11, 55);
+  await compare(page, 11, 90);
+});
+
 test("15(残響散歌)", async ({ page }) => {
   await compare(page, 15, 50);
+  await compare(page, 15, 90);
+  await compare(page, 15, 105);
+});
+
+test("22(ヨワイボクラハウタウ)", async ({ page }) => {
+  await compare(page, 22, 140);
 });
 
 const compare = async (page: Page, video: number, time: number) => {

652c26d477e69a1b25a022098fe7e29c234bbb9a
xpa <xpadev@gmail.com>
2023-05-06 00:03:25 +0900

    fix: release workflow

---
+++ rollup.config.dts.mjs
@@ -0,0 +1,22 @@
+import pkg from "./package.json" assert {type: 'json'};
+import { fileURLToPath } from 'url';
+import * as path from "path";
+import dts from "rollup-plugin-dts";
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+const banner = `/*!
+  niconicomments.js v${pkg.version}
+  (c) 2021 xpadev-net https://xpadev.net
+  Released under the ${pkg.license} License.
+*/`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+export default {
+    input: "./dist/dts/main.d.ts",
+    output: [{ file: "dist/bundle.d.ts", format: "es" }],
+    plugins: [
+        typescriptPaths(),
+        dts()
+    ],
+};

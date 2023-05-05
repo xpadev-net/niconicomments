38bc6e5fc82eb08256a30bb67ff8a941ebd75020
xpa <xpadev@gmail.com>
2023-05-06 00:43:47 +0900

    fix: build process

--- util/resolve-path-alias.js
+++ util/resolve-path-alias.js
@@ -11,7 +11,6 @@ const processDir = (dir = "./dist/dts", deps = 0) => {
 
       const content = fs
         .readFileSync(path, "utf-8")
-        .replace(/import (?:type )?[\s\S]*? from "@\/@types.*?";/g, "")
         .replace(/@\//g, "./" + "../".repeat(deps))
         .replace(/\/\/# sourceMappingURL=.+\.d\.ts\.map/g, "");
       fs.writeFileSync(path, content, "utf-8");
